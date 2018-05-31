4a63b81589c17585b3686051edc3a49266adbb40
Booz <booz@rezo.net>
2018-05-31 22:53:51 +0200

    raccourcis typographiques de grilles

---
+++ listes_de_lecture/morceaux.js
@@ -0,0 +1,35 @@
+/*.playliste */
+
+var liste_de_lecture = {
+
+"01": {
+	"titre" : "Majeur",
+	"accompagement" : `
+|| Choirs ||
+|| Basse ||
+|| Batterie ||
+|| 4/4 ||
+||:	Dm7	|	G7	|	Cmaj7	|	Cmaj7	  |
+|	B7	|	E7	|	Am7 	|	Am7 	:||
+||:	Dm7	|	G7	|	Cmaj7	|	Cmaj7	  |
+|	B7	|	E7	|	Am7 	|	Am7 	:||
+`},
+
+"02": {
+		"titre" : "Mixolydien",
+		"accompagement" : `
+|| Piano ||
+|| Basse ||
+|| Batterie ||
+|| 4/4 ||
+||:	F7	|	G7	|	C7	|	C7	  |
+|	B7	|	E7	|	Am7	|	Am7	:||
+||:	F7	|	G7	|	C7	|	C7	  |
+|	B7	|	E7	|	Am7	|	Am7	:||
+||:	F7	|	G7	|	C7	|	C7	  |
+|	B7	|	E7	|	Am7	|	Am7	:||
+||:	F7	|	G7	|	C7	|	C7	  |
+|	B7	|	E7	|	Am7	|	Am7	:||
+`},
+
+}
